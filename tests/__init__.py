083f841a148243f36692942ea671fde63b0af4ee
Harshad Hegde <hegdehb@gmail.com>
2022-06-30 18:05:09 -0500

    got kgcl module in path

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,10 @@
 import os
 import pprint
+import sys
+from os.path import dirname, join
 
 EXAMPLE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "examples")
 RESOURCE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "resources")
 TARGET_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "target")
+
+sys.path.append(join(dirname(dirname(__file__)), "src/"))
