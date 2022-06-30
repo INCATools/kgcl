13519da6d313f44504419258be625a900fcaa58b
cmungall <cjm@berkeleybop.org>
2022-06-30 14:51:31 -0700

    will remove

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import os
+import pprint
+
+EXAMPLE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "examples")
+RESOURCE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "resources")
+TARGET_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "target")
