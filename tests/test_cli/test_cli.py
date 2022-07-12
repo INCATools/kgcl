44a20f07d0c6676e3f6f4a0ccf78951ad07c6df0
Chris Mungall <cjm@berkeleybop.org>
2022-07-12 15:03:51 -0700

    Removing packages now in kgcl-rdflib (#5)
    * Removing packages now in kgcl-rdflib
    
    * adding missing

--- tests/test_cli/test_cli.py
+++ tests/test_cli/test_cli.py
@@ -1,16 +1,13 @@
 import logging
 import unittest
 
-import rdflib
 from click.testing import CliRunner
 
-import kgcl_schema.kgcl as kgcl_apply
-import kgcl_schema.kgcl_diff as kgcl_diff
 import kgcl_schema.grammar.parser as kgcl_parser
 from kgcl_schema.datamodel.kgcl import Session
 from kgcl_schema.utils import from_yaml
 
-from tests import INPUT, TMP_OUTPUT, DIFF_OUTPUT, DIFF_OUTPUT_DIR, TMP_YAML
+from tests import TMP_YAML
 from tests.cases import CASES, TODO_TOKEN, UID
 
 
@@ -22,18 +19,6 @@ class CliTestSuite(unittest.TestCase):
     def setUp(self) -> None:
         runner = CliRunner(mix_stderr=False)
         self.runner = runner
-
-    def test_apply_help(self):
-        result = self.runner.invoke(kgcl_apply.cli, ["--help"])
-        out = result.stdout
-        self.assertIn("--graph", out)
-        self.assertIn("PATCH", out)
-        self.assertEqual(0, result.exit_code)
-
-    def test_diff_help(self):
-        result = self.runner.invoke(kgcl_diff.cli, ["--help"])
-        out = result.stdout
-        self.assertEqual(0, result.exit_code)
 
     def test_parse_help(self):
         result = self.runner.invoke(kgcl_parser.cli, ["--help"])
@@ -52,40 +37,5 @@ class CliTestSuite(unittest.TestCase):
         out = result.stdout
         self.assertEqual(0, result.exit_code)
 
-    def test_diff_to_empty(self):
-        g = rdflib.Graph()
-        g.serialize(destination=TMP_OUTPUT)
-        diff_result = self.runner.invoke(
-            kgcl_diff.cli, [INPUT, TMP_OUTPUT, '-o', DIFF_OUTPUT, '-d', DIFF_OUTPUT_DIR]
-        )
-        self.assertEqual(0, diff_result.exit_code)
-
-    def test_cases(self):
-        """Test CLI on each case."""
-        for patch, expected_diff, _, _ in CASES:
-            parse_result = self.runner.invoke(
-                kgcl_parser.cli, ['-o', TMP_OUTPUT, patch]
-            )
-            #from_yaml
-            self.assertEqual(0, parse_result.exit_code)
-            apply_result = self.runner.invoke(
-                kgcl_apply.cli, ['--graph', INPUT, '-o', TMP_OUTPUT, patch]
-            )
-            self.assertEqual(0, apply_result.exit_code)
-            diff_result = self.runner.invoke(
-                kgcl_diff.cli, [INPUT, TMP_OUTPUT, '-o', DIFF_OUTPUT, '-d', DIFF_OUTPUT_DIR]
-            )
-            self.assertEqual(0, diff_result.exit_code)
-            changes = [line.strip() for line in (open(DIFF_OUTPUT).readlines())]
-            if expected_diff is None:
-                self.assertGreater(len(changes), 0)
-                logging.warning(f"TODO: cases is under-specified here. Patch({patch}) ==> {changes}")
-            elif isinstance(expected_diff, list):
-                self.assertCountEqual(expected_diff, changes)
-            elif expected_diff == TODO_TOKEN:
-                pass
-            else:
-                self.assertEqual(1, len(changes))
-                self.assertEqual(changes[0], expected_diff)
 
 
--- tests/util.py
+++ tests/util.py
@@ -2,43 +2,6 @@
 from linkml_runtime.dumpers import json_dumper
 from linkml_runtime.loaders import json_loader
 from linkml_runtime.utils.yamlutils import YAMLRoot
-from rdflib import Graph
-from rdflib.compare import graph_diff, to_isomorphic
-
-from kgcl_schema.apply.graph_transformer import apply_patch
-from kgcl_schema.grammar.parser import parse
-
-
-def compare_graphs(actual, expected):
-    """Compare actual and expected graphs."""
-    actual_iso = to_isomorphic(actual)
-    expected_iso = to_isomorphic(expected)
-
-    if actual_iso != expected_iso:
-        _, in_first, in_second = graph_diff(actual_iso, expected_iso)
-        print("The actual and expected graphs differ")
-        print("----- Contents of actual graph not in expected graph -----")
-        print(in_first.serialize(format="nt").decode("utf-8"))
-        # dump_ttl_sorted(in_first)
-        print("----- Contents of expected graph not in actual graph -----")
-        print(in_second.serialize(format="nt").decode("utf-8"))
-        # dump_ttl_sorted(in_second)
-
-    assert actual_iso == expected_iso # commented by H2.
-
-
-def run_test(input_graph, kgcl_patch, expected_graph):
-    """Run tests."""
-    # load input graph
-    g = Graph().parse(data=input_graph, format="nt")
-
-    # load expected output graph
-    expected = Graph().parse(data=expected_graph, format="nt")
-
-    parsed_patch = parse(kgcl_patch)
-    apply_patch(parsed_patch, g)
-
-    compare_graphs(g, expected)
 
 
 def roundtrip(obj: YAMLRoot, format='json'):
