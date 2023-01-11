6bbeac953eca127493e02727d97b30c0a8c2ebad
Harshad Hegde <hegdehb@gmail.com>
2023-01-11 16:24:31 -0600

    fixed bizarre error

--- tests/test_case_dumpers.py
+++ tests/test_case_dumpers.py
@@ -20,7 +20,7 @@ import kgcl_schema.grammar.parser as kgcl_parser
 from kgcl_schema.utils import from_yaml
 from tests import TMP_YAML
 from tests.cases import CASES
-from linkml_runtime.dumpers import yaml_dumper, json_dumper
+from linkml_runtime.dumpers import yaml_dumper
 import yaml
 from kgcl_schema.grammar.parser import parse_statement
 from pathlib import Path
@@ -32,34 +32,34 @@ YAML_OUT_FN = "examples.yaml"
 YAML_OUT_PATH = SRC_DIR / "data/examples" / YAML_OUT_FN
 MD_OUT_PATH = SRC_DIR / "docs" / MD_OUT_FN
 
+def export_yaml_and_md():
+    yaml_dict = {}
+    used_type = {}
+    md = "# KGCL Commands.\n\n"
+    # Dump YAML file with test information.
+    for idx, (command_curie, command_uri, obj, _) in enumerate(CASES):            
+        yaml_dict[f"Test_{idx}"] = obj.__dict__.copy()
+        yaml_dict[f"Test_{idx}"]["type"] = type(obj).__name__
+        yaml_dict[f"Test_{idx}"]["command_with_curie"] = command_curie
+        yaml_dict[f"Test_{idx}"]["command_with_uri"] = command_uri
 
+        if type(obj).__name__ not in used_type:
+            used_type[type(obj).__name__] = 1
+            md += f"## `{type(obj).__name__}`:\n"
+            md += f"\tCommand using CURIEs: `{command_curie}`\n"
+            if command_uri and command_uri != "TODO":
+                md += f"\tCommand using URIs: `{command_uri}`\n\n"
+            else:
+                md += "\n"
+
+        yaml_dumper.dump(yaml_dict, YAML_OUT_PATH)
+        with open(MD_OUT_PATH, "w") as md_dump:
+            md_dump.write(md)
 class TestCases(unittest.TestCase):
     def setUp(self) -> None:
         runner = CliRunner(mix_stderr=False)
         self.runner = runner
-        self.md = "# KGCL Commands.\n\n"
-        used_type = {}
-
-        self.yaml_dict = {}
-        # Dump YAML file with test information.
-        for idx, (command_curie, command_uri, obj, _) in enumerate(CASES):
-            obj.__dict__["type"] = type(obj).__name__
-            obj.__dict__["command_with_curie"] = command_curie
-            obj.__dict__["command_with_uri"] = command_uri
-            self.yaml_dict[f"Test_{idx}"] = obj.__dict__
-
-            if type(obj).__name__ not in used_type:
-                used_type[type(obj).__name__] = 1
-                self.md += f"## `{type(obj).__name__}`:\n"
-                self.md += f"\tCommand using CURIEs: `{command_curie}`\n"
-                if command_uri and command_uri != "TODO":
-                    self.md += f"\tCommand using URIs: `{command_uri}`\n\n"
-                else:
-                    self.md += "\n"
-
-        yaml_dumper.dump(self.yaml_dict, YAML_OUT_PATH)
-        with open(MD_OUT_PATH, "w") as md_dump:
-            md_dump.write(self.md)
+        export_yaml_and_md()
 
     def test_case_yaml_dumper(self):
         self.assertTrue(YAML_OUT_PATH.is_file())
