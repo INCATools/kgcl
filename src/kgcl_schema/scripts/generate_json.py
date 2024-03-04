5cf2ba6897068e6e59ac8df806fdf1d9b4010b72
caufieldjh <j.harry.caufield@gmail.com>
2024-03-04 17:59:14 -0500

    remove predicate traversal

--- src/kgcl_schema/scripts/generate_json.py
+++ src/kgcl_schema/scripts/generate_json.py
@@ -47,42 +47,6 @@ def get_tree_class_recursive(root_node: dict, parent_to_child_map: dict) -> dict
     return root_node
 
 
-def load_predicate_tree_data(
-    return_parent_to_child_dict: bool = False,
-) -> Union[List[List[Any]], dict]:
-    """
-    Load the predicate tree data from the model.
-
-    :return: The predicate tree data.
-    """
-    parent_to_child_dict = defaultdict(set)
-    predicate_tree = []
-    for slot_name in sv.all_slots(imports=True):
-        slot = sv.get_slot(slot_name)
-        if slot.deprecated:
-            continue
-        parent_name_english = slot.is_a
-        if parent_name_english:
-            parent_name = parent_name_english
-            parent_to_child_dict[parent_name].add(slot_name)
-            root_node = {"name": "related_to"}
-            predicate_tree = get_tree_slot_recursive(root_node, parent_to_child_dict)
-        if slot.mixins:
-            for mixin in slot.mixins:
-                parent_name_english = mixin
-                parent_name = parent_name_english
-                parent_to_child_dict[parent_name].add(slot_name)
-                root_node = {"name": "related_to_at_instance_level"}
-                predicate_tree = get_tree_slot_recursive(
-                    root_node, parent_to_child_dict
-                )
-    return (
-        ([predicate_tree], parent_to_child_dict)
-        if return_parent_to_child_dict
-        else ([predicate_tree])
-    )
-
-
 def load_category_tree_data(return_parent_to_child_dict: bool = False) -> tuple:
     """
     Load the category tree data from the model.
@@ -164,11 +128,7 @@ def get_tree_slot_recursive(root_node: dict, parent_to_child_map: dict) -> dict:
 def generate_viz_json():
 
     # Generate the d3 viz data
-    pred_data = load_predicate_tree_data()
     cat_data = load_category_tree_data()
-
-    with open("src/docs/predicates.json", "w") as json_file:
-        json.dump(pred_data, json_file, indent=4)
 
     with open("src/docs/categories.json", "w") as json_file:
         json.dump(cat_data, json_file, indent=4)
